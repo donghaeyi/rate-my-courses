79fa72a0145e07f9c8d9ea212a6c08d5bec2e9e8
Matt <matt.harper3415@gmail.com>
2024-04-02 14:14:09 -0600

    Preliminary implementation of reactive search box

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -1,6 +1,7 @@
 //  Import dependencies
 const express = require("express");
 const app = express(); // Create Express app
+const axios = require("axios").default;
 
 const handlebars = require("express-handlebars");
 
@@ -21,6 +22,52 @@ app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
   res.render("pages/home", {
     text: "Hello world!",
+  });
+});
+
+// API route to return the appropriate class suggestions from a keyword search
+// Request: requires query parameter "keyword" which represents user search terms
+//              e.g. "CSCI 2270" or "robotics" or "ASEN"
+// Returns: list of matching courses, each an object with title and code
+app.get("/search", async (req, res) => {
+  let data = JSON.stringify({
+    "other": {
+      "srcdb": "2247" //todo get current term for use
+    },
+    "criteria": [
+      {
+        "field": "keyword",
+        "value": req.query.keyword
+      }
+    ]
+  });
+  
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: `https://classes.colorado.edu/api/?page=fose&route=search&keyword=${req.query.keyword}`,
+    headers: { 
+      'Content-Type': 'application/json'
+    },
+    data: data
+  };
+  
+  axios.request(config)
+  .then((response) => {
+    const apidata = response.data.results
+
+    let data = []
+    for (const datum of apidata) {
+      if(!data.some(d => d.title == datum.title)) data.push({
+        title: datum.title,
+        code: datum.code
+      })
+    }
+
+    res.send(data)
+  })
+  .catch((error) => {
+    console.log(error);
   });
 });
 
