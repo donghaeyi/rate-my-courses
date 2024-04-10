53768a5d0b6170b6bf57b7bd1b6712a726bff43e
Maddie Kloud <madeline.kloud@gmail.com>
2024-04-10 13:53:50 -0600

    A few commits behind but saving my work

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -198,38 +198,44 @@ app.get('/review', (req, res) => {
   });
   
 });
+
+//Write a new review (adds review to reviews table)
 //assuming that the user can press a button on the nav bar to write a review about a class (or we could have this built into the course page)
-app.post('review/addReview', async function (req, res) {
+app.post('/addReview', async function (req, res) {
   try{
     //user won't be able to access the review form if they are not logged in, this route takes care of the submit review action
-    const user_id = req.session.user.user_id;
-
-    //query to find the course that they are writing the review for. For simplicity, I'm going to have the user input the course code (like CSCI) and the course number to find the course id
-    //const course_id = await db.one('SELECT course_id FROM courses WHERE course_number = ($1) AND course_tag = ($2)', [req.body.course_number, req.body.course_tag]);
+    const user_id = parseInt(req.session.user.user_id);
     //if the user can only select courses from a drop down menu, we can ensure that we get a course id from each request
-    const course_id = req.body.course_id;
+    const course_id = await db.one('SELECT course_id FROM courses WHERE course_name = ($1);',[req.body.course_name]);
     //query professors by a dropdown menu listing all of the professors for matched course
-    const professor_id = req.body.professor_id;
+    let input_professor = req.body.professor; //since professor was displayed as first name and last name combined in the drop down, need to split into first name and last name to query the table
+    let split_name = input_professor.split(" ");
+    const first_name = split_name[0];
+    const last_name = split_name[1];
+    const professor_id = await db.one('SELECT professor_id FROM professors WHERE first_name = ($1) AND last_name = ($2);', [first_name, last_name]); //name 
+    
     //need to make different routes so that the user can add metrics, this might be hard to accomodate all combinations
     
-    const review = await db.one(`INSERT INTO reviews (course_id, year_taken, term_taken, user_id, overall_rating, review, professor_id) values ($1, $2, $3, $4, $5, $6, $7) returning review_id;`, 
+    const review = await db.one(`INSERT INTO reviews (course_id, year_taken, term_taken, user_id, review, overall_rating, professor_id) values ($1, $2, $3, $4, $5, $6, $7) returning review_id;`, 
     [course_id, 
-    req.body.year_taken, 
+    parseInt(req.body.year_taken), 
     req.body.term_taken, 
     user_id, 
-    req.body.overall_rating,
     req.body.review,
-    req.body.professor_id
+    parseInt(req.body.overall_rating),
+    professor_id
     ]);
     res.status(201).json({
       status: "success",
       review_id: review,
       message: "Data added successfully"
     });
+    
   }catch(error){
     console.log(error);
-    res.status(500).send(); //right now it'll log an error if either the course isn't found or if all of the required data  isn't sent
+    res.status(500).send();
   }
+    
 });
 
 module.exports = app.listen(3000);
